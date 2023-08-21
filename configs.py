0e5d873adaf8ed0bf9845fd54985217671a6622b
yukaneko <yukaneko@image.u-aizu.ac.jp>
2023-08-21 21:07:37 +0900

    fix config

--- configs.py
+++ configs.py
@@ -88,11 +88,11 @@ def build_config(dataset):
     # base settings
     cfg.feat_dim = 1024
     cfg.head_num = 1
-    cfg.hid_dim = 32
+    cfg.hid_dim = 128
     cfg.out_dim = 300
     cfg.lr = 5e-4
     cfg.dropout = 0.1
-    cfg.train_bs = 128
+    cfg.train_bs = 32
     cfg.max_seqlen = 200
     cfg.max_epoch = 100
     cfg.workers = 8
