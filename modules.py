0ce7e9112aa7e480d4635de5908471ee6e4ca4c6
yukaneko <yukaneko@image.u-aizu.ac.jp>
2024-02-22 12:11:40 +0900

    :sparkles: ablation study 2

--- modules.py
+++ modules.py
@@ -21,9 +21,9 @@ class XEncoder(nn.Module):
         self.dropout2 = Pdropout(dropout)
         self.norm = nn.LayerNorm(d_model)
         self.loc_adj = DistanceAdj(gamma, bias)
-        self.UR_DMU = WSAD(d_model, a_nums = a_nums, n_nums = n_nums, dropout = dropout)
-        self.hard_atten = HardAttention(k=0.95, num_samples=100, input_dim=d_model//2)
-        self.conv1 = nn.Conv1d(d_model, d_model // 2, kernel_size=1)
+        # self.UR_DMU = WSAD(d_model, a_nums = a_nums, n_nums = n_nums, dropout = dropout)
+        # self.hard_atten = HardAttention(k=0.95, num_samples=100, input_dim=d_model//2)
+        # self.conv1 = nn.Conv1d(d_model, d_model // 2, kernel_size=1)
         self.dropout = nn.Dropout(0.05)
         assert d_model // 2 == 512
         
@@ -33,30 +33,30 @@ class XEncoder(nn.Module):
         adj = self.loc_adj(x.shape[0], x.shape[1])
         mask = self.get_mask(self.win_size, x.shape[1], seq_len)
         
-        x_h = self.hard_atten(c_x)
+        # x_h = self.hard_atten(c_x)
         
         x = x + self.self_attn(x, mask, adj)
         x_t = x
         
-        x = x.permute(0, 2, 1)
-        x = F.relu(self.conv1(x))
-        x = x.permute(0, 2, 1)
-        x = self.dropout(x)
-        x_v = x
+        # x = x.permute(0, 2, 1)
+        # x = F.relu(self.conv1(x))
+        # x = x.permute(0, 2, 1)
+        # x = self.dropout(x)
+        # x_v = x
         
-        x = torch.cat((x, x_h), -1)
+        # x = torch.cat((x, x_h), -1)
         
         # x = self.norm(x)
-        x_k = self.UR_DMU(x)
-        x = x_k["x"]
+        # x_k = self.UR_DMU(x)
+        # x = x_k["x"]
     
-        x = x + x_t
+        # x = x + x_t
         
         x = self.norm(x).permute(0, 2, 1)
-        x = self.dropout1(F.gelu(self.linear1(x) + x_v.permute(0, 2, 1)))
+        x = self.dropout1(F.gelu(self.linear1(x))) #+ x_v.permute(0, 2, 1)))
         x_e = self.dropout2(F.gelu(self.linear2(x)))
         
-        # x_k = dict()
+        x_k = dict()
         
         if self.training:
             x_k["x"] = x
